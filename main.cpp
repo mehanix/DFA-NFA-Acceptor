f8d1d0a67bdbd1b85d05214281550dd1190c8682
mehanix <nixikunn@gmail.com>
2020-03-15 14:24:43 +0200

    work on big automata

--- main.cpp
+++ main.cpp
@@ -45,12 +45,22 @@ void afiseaza_graf(GRAF graf, int nr_stari) {
  * graF: graful automatului
  * stari_finale: vectorul de stari finale
  * */
-void verify(string& word, int nod_curent, int i_char_curent, const int i_char_final, GRAF& graf, int current_lg, vector<int>& stari_finale, vector<vector<bool>> matrice_vizitati) {
+void verify(string& word, int nod_curent, int i_char_curent, const int i_char_final, GRAF& graf, int current_lg, vector<int>& stari_finale, vector<vector<bool>>& matrice_vizitati) {
     ///daca nu s-a mai ajuns aici cu lungimea asta, mergi pe aici///
-	cout<<"am ajuns aici";
-    if(matrice_vizitati[nod_curent][current_lg] == false && is_valid == 0) {
-    	matrice_vizitati[nod_curent][current_lg] = true;
-        cout<<"stare curenta:"<<nod_curent<<'\n';
+	/*cout<<"\n\n";
+	for(vector<bool> &v:matrice_vizitati) {
+    		v.resize(word.length()+1);
+    		for (auto x:v){
+    			cout<<x<<' ';
+    		}
+    		cout<<'\n';
+    	}*/
+
+	//cout<<"\n\n";
+
+	if(matrice_vizitati[nod_curent][current_lg] == false && is_valid == 0) {
+    	//matrice_vizitati[nod_curent][current_lg] = true;
+        //cout<<"stare curenta:"<<nod_curent<<'\n';
 
         /*
          * Conditie de iesire!!
@@ -70,7 +80,6 @@ void verify(string& word, int nod_curent, int i_char_curent, const int i_char_fi
 
         /// Verifica de unde poti pleca din nodul nod_curent cu caracterul word[i_char_curent] ///
         /// itereaza prin vectorul dat de graf[nod_curent][word[i_char_curent]] si reapeleaza functia cu caracterul urmator ///
-
         char c = word[i_char_curent];
         cout<<"Din litera "<<c<<" si nodul "<<nod_curent<<" ajungi in:";
         for(int &ends : graf[nod_curent][c]) {
@@ -78,12 +87,20 @@ void verify(string& word, int nod_curent, int i_char_curent, const int i_char_fi
         }
         cout<<'\n';
 
-        for(auto &ends : graf[nod_curent][c]) {
+        cout<<graf[nod_curent][c].size()<<"        ";
+      //  if (graf[nod_curent][c].size()>0){
+            for(auto &ends : graf[nod_curent][c]) {
+                //cout<<"graf[nod_curent][c] e "<<matrice_vizitati[nod_curent][current_lg];
+                if(matrice_vizitati[nod_curent][current_lg] == false){
+                    cout<<"Plec pe "<<c<<" "<<ends<<endl;
+                    matrice_vizitati[nod_curent][current_lg] = true;
+                    verify(word, ends,i_char_curent+1, i_char_final, graf,current_lg+1, stari_finale, matrice_vizitati);
+                    matrice_vizitati[nod_curent][current_lg] = false;
+                }
 
-            cout<<"Plec pe "<<c<<" "<<ends<<endl;
-            verify(word, ends,i_char_curent+1, i_char_final, graf,current_lg+1, stari_finale, matrice_vizitati);
+            }
+      //  }
 
-        }
 
         /// Daca trece prin tot graful si nu reuseste sa gaseasca un drum care sa accepte cuvantul, atunci e neacceptat ///
 
@@ -112,18 +129,13 @@ void verify(string& word, int nod_curent, int i_char_curent, const int i_char_fi
 }
 
 
-int main()
-{
-    int nr_stari, nr_tranzitii, stare_initiala, nr_stari_finale;
-    /// citire automat ///
+
+
+void load_from_file(GRAF &graf, int &nr_stari, int &nr_tranzitii, int &stare_initiala, int &nr_stari_finale, vector<int>& stari_finale) {
+/// citire automat ///
     //cout<<"Nr. stari:";
-    fin>>nr_stari;
-    fout<<"lala";
-
-    GRAF graf(nr_stari+1);
-
     fin>>nr_tranzitii;
-
+    cout<<nr_stari<<nr_tranzitii;
     for(int i=0;i<nr_tranzitii;i++) {
         //cout<<"Introdu tranzitia "<<i<<". [format:q1 q2 ch]\n";
         int q1, q2; char c;
@@ -134,7 +146,7 @@ int main()
     fin>>stare_initiala;
     fin>>nr_stari_finale;
 
-    vector<int>stari_finale(nr_stari_finale);
+    stari_finale.resize(nr_stari_finale);
     for(int i=0;i<nr_stari_finale;i++) {
         fin>>stari_finale[i];
     }
@@ -143,30 +155,97 @@ int main()
         cout<<stari_finale[i]<<' ';
     }
     cout<<'\n';
-    afiseaza_graf(graf,nr_stari);
+
+
+}
+
+void load_big(GRAF &graf, int &nr_stari, int &nr_tranzitii, int &stare_initiala, int &nr_stari_finale, vector<int>& stari_finale){
+    nr_stari = 3004;
+    graf.resize(nr_stari+1);
+    stare_initiala = 3001;
+    nr_tranzitii = 0;
+    nr_stari_finale=1;
+    stari_finale.resize(nr_stari_finale);
+    stari_finale[0] = 3004;
+    //adaug muchii 3001 - [1..1000] 'a' (1000)
+    for(int i=1;i<=1000;i++) {
+        nr_tranzitii++;
+        adauga_arc(graf, 3001, i, 'a');
+    }
+    //adaug muchii [1...1000] -> [1001...2000] (1mil)
+    for(int i=1;i<=1000;i++)
+        for (int j=1001;j<=2000;j++) {
+            nr_tranzitii++;
+            adauga_arc(graf,i,j,'b');
+        }
+    //adaug muchii [1001-2000]...[2001-3000] (1mil)
+    for(int i=1001;i<=2000;i++)
+        for (int j=2001;j<=3000;j++) {
+            nr_tranzitii++;
+            adauga_arc(graf,i,j,'c');
+        }
+    //adaug muchii [2001-3000] - 3002 'd' (1000)
+    for(int i=2001;i<=3000;i++) {
+        nr_tranzitii++;
+        adauga_arc(graf, i, 3002, 'd');
+    }
+    nr_tranzitii++;
+    adauga_arc(graf,3000,3003,'d');
+    nr_tranzitii++;
+    adauga_arc(graf,3003,3004,'e');
+
+
+}
+
+int main()
+{
+    vector<int>stari_finale;
+    vector<vector<bool>> matrice_vizitati;
+    int nr_stari, nr_tranzitii, stare_initiala, nr_stari_finale;
+    int choice=-1;
+    GRAF graf;
+    while(choice==-1){
+        cout<<"Alege o optune:\n1.Incarca graf custom din fisier\n2.Incarca graful mare din cod (cel cu 3000 noduri si 2002002 muchii)\n\nScrie numarul optiunii alese:";
+        cin>>choice;
+        if (choice == 1) {
+            fin>>nr_stari;
+            graf.resize(nr_stari+1);
+            load_from_file(graf, nr_stari, nr_tranzitii,stare_initiala,nr_stari_finale, stari_finale);
+        }
+        else if (choice == 2)
+            load_big(graf, nr_stari, nr_tranzitii,stare_initiala,nr_stari_finale, stari_finale);
+        else choice = -1;
+    }
+
+
+
 
     string word;
     cout<<"Automat incarcat din fisier cu succes! :)"<<endl;
-    while (word != "EXIT" && word !="exit") {
+    cout<<nr_stari_finale<<' '<<stari_finale[0];
+    while (true) {
     	cout<<"Introdu cuvantul de verificat:[sau EXIT pentru a iesi]"<<endl;
     	cin>>word;
-
+    	if(word == "EXIT" || word =="exit")
+            break;
+        cout<<"Working on it...\n";
 
     	//creez matrice vizitati pt cuv asta
     	//lungimea max = lungimea cuvantului
-    	vector<vector<bool>> matrice_vizitati(nr_stari+1);
+
+    	matrice_vizitati.resize(nr_stari+1);
     	for(vector<bool> &v:matrice_vizitati) {
     		v.resize(word.length()+1);
-    		for (auto x:v){
+    		for (auto&& x:v){
     			x=false;
     		}
-    	}
 
-    	is_valid=0;
-    	verify(word,stare_initiala,0,word.length(),graf,0,stari_finale,matrice_vizitati);
+    	}
+        verify(word,stare_initiala,0,word.length(),graf,0,stari_finale,matrice_vizitati);
     	if(is_valid)
     		cout<<"Cuvant acceptat de automat!\n";
     	else cout<<"Cuvant neacceptat de automat!\n";
+    	is_valid=0;
 
     	}
     	cout<<"Goodbye!";
